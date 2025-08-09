1b2722169b97b357e52be37670c8f09222d96aa3
Akram Shaikh <138965478+akram10shaikh@users.noreply.github.com>
2025-08-09 15:51:16 +0530

    commit

--- Internship_pro/settings.py
+++ Internship_pro/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['Internship_pro.onrender.com']
 
 
 # Application definition
