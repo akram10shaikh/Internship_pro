9d88d0349d236851bf162b9aad5dea7dceb5c665
Akram Shaikh <138965478+akram10shaikh@users.noreply.github.com>
2025-08-14 15:49:56 +0530

    ab commit

--- Internship_pro/settings.py
+++ Internship_pro/settings.py
@@ -142,11 +142,11 @@ DEFAULT_FROM_EMAIL = 'noreply@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.hostinger.com'
 EMAIL_PORT = 465  
-# EMAIL_USE_TLS = True
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'harish@prspera.com' #this uses company smtp mail id
 EMAIL_HOST_PASSWORD = 'BlueBoxer512i!' #this uses company smtp app password
 DEFAULT_FROM_EMAIL = 'noreply@gmail.com'
-EMAIL_USE_SSL = True
+#EMAIL_USE_SSL = True
 
 AUTH_USER_MODEL = 'Myapp.CustomUser'
 
