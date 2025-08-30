226b4bfe2038ee9a0d867714c4470bf78e6a397c
Akram Shaikh <138965478+akram10shaikh@users.noreply.github.com>
2025-08-30 14:24:06 +0530

    addding it

--- Internship_pro/settings.py
+++ Internship_pro/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
-
+from decouple import config
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
