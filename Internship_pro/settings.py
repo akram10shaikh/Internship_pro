4a68a2114b6df2fe27e1ede59b39667a4a231a89
Akram Shaikh <138965478+akram10shaikh@users.noreply.github.com>
2025-08-30 15:59:02 +0530

    server is added

--- Internship_pro/settings.py
+++ Internship_pro/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['31.97.117.28']
 
 
 # Application definition
